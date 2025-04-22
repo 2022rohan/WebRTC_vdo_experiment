22e625bb6ad8b922e8fdfe5d75d7d6704197be2e
Rohan Lather <rohanlather2010@gmail.com>
2025-04-22 15:33:49 +0530

    eslint changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    eslint: {
+      ignoreDuringBuilds: true, // 👈 disables ESLint checks at build time
+    },
+  };
 
 export default nextConfig;
